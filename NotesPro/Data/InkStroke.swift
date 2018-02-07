5d3e1bdd6929c3dbf0ece398f6e23c466de307ba
Dominik Horn <horn.dominik101@yahoo.de>
2018-02-07 17:47:13 +0100

    Add experimental Quadratic curve bezier drawing

--- NotesPro/Data/InkStroke.swift
+++ NotesPro/Data/InkStroke.swift
@@ -29,14 +29,38 @@ class InkStroke {
     
     func getBezierPath() -> UIBezierPath {
         let path = UIBezierPath()
-        if let fp = points.first {
-            path.move(to: fp)
-            for p in points {
-                path.addLine(to: p)
+        if var p1 = points.first {
+            path.move(to: p1)
+            
+            for i in 1..<points.count {
+                let p2 = points[i]
+                let midPoint = getMidPoint(p1, p2)
+                
+                path.addQuadCurve(to: midPoint, controlPoint: getControlPoint(midPoint, p1))
+                path.addQuadCurve(to: p2, controlPoint: getControlPoint(midPoint, p2))
+                
+                p1 = p2
             }
             path.lineWidth = linewidth;
         }
         
         return path
     }
+}
+
+func getMidPoint(_ p1: CGPoint, _ p2: CGPoint) -> CGPoint {
+    return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
+}
+
+func getControlPoint(_ p1: CGPoint, _ p2: CGPoint) -> CGPoint {
+    var controlPoint = getMidPoint(p1, p2)
+    let diffY = abs(p2.y - controlPoint.y)
+    
+    if p1.y < p2.y {
+        controlPoint.y += diffY
+    } else if p1.y > p2.y {
+        controlPoint.y -= diffY
+    }
+    
+    return controlPoint
 }
