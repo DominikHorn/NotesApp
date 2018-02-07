5d620d4d066733a73d6db8158f6e5051091d0f38
Dominik Horn <horn.dominik101@yahoo.de>
2018-02-07 15:14:26 +0100

    Initial Commit

---
+++ NotesPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotesPro
+//
+//  Created by Dominik Horn on 07.02.18.
+//  Copyright © 2018 Dominik Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
