1b7296ea8dc77778f1c914657ffb7141fdd96a46
Dominik Horn <horn.dominik101@yahoo.de>
2018-02-08 18:25:20 +0100

    Implement width picking

--- NotesApp/Views/PencilPickerViewController.swift
+++ NotesApp/Views/PencilPickerViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController {
+class PencilPickerViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var widthSlider: UISlider!
     @IBOutlet weak var colorDisplay: UIView!
-    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -25,24 +25,27 @@ class ColorPickerViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         // TODO: sync with documentViewController
-        colorUpdated()
+        update()
     }
     
     // MARK: -
     // MARK: UIActions
     @IBAction func sliderChanged(_ sender: UISlider) {
         currentColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
-        colorUpdated()
+        currentLineWidth = CGFloat(widthSlider.value)
+        update()
     }
     
     // MARK: -
     // MARK: helper
-    func colorUpdated(_ animated: Bool = false) {
+    func update(_ animated: Bool = false) {
         if let colors = currentColor.cgColor.components {
             redSlider.setValue(Float(colors[0]), animated: animated)
             greenSlider.setValue(Float(colors[1]), animated: animated)
             blueSlider.setValue(Float(colors[2]), animated: animated)
         }
+        
+        widthSlider.setValue(Float(currentLineWidth), animated: animated)
         
         colorDisplay.backgroundColor = currentColor
         colorDisplay.setNeedsDisplay()
