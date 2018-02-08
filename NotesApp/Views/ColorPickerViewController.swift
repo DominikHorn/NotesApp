9042f4c5de27b6e69328df6add0326d378a6a289
Dominik Horn <horn.dominik101@yahoo.de>
2018-02-08 18:10:35 +0100

    Implement color picker view

--- NotesApp/Views/ColorPickerViewController.swift
+++ NotesApp/Views/ColorPickerViewController.swift
@@ -8,51 +8,42 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController, ColorPickerDelegate {
+class ColorPickerViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorDisplay: UIView!
     
-    var currentColor: UIColor {
-        set {
-            colorUpdated()
-        }
-        get {
-            return self.currentColor
-        }
-    }
+    var currentColor: UIColor?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
-        // TODO: sync with documentViewController
-        currentColor = UIColor.green
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        colorUpdated(true)
+        // TODO: sync with documentViewController
+        currentColor = UIColor.green
+        colorUpdated()
     }
     
     // MARK: -
     // MARK: UIActions
     @IBAction func sliderChanged(_ sender: UISlider) {
         currentColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+        colorUpdated()
     }
     
     // MARK: -
     // MARK: helper
     func colorUpdated(_ animated: Bool = false) {
-        if let colors = currentColor.cgColor.components {
-            redSlider.setValue(Float(colors[0] * 255), animated: animated)
-            greenSlider.setValue(Float(colors[1] * 255), animated: animated)
-            blueSlider.setValue(Float(colors[2] * 255), animated: animated)
+        if let colors = currentColor?.cgColor.components {
+            redSlider.setValue(Float(colors[0]), animated: animated)
+            greenSlider.setValue(Float(colors[1]), animated: animated)
+            blueSlider.setValue(Float(colors[2]), animated: animated)
         }
         
         colorDisplay.backgroundColor = currentColor
