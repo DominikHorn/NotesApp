64f8f6786c3a55fa9cafd28ebce988f155fe29c1
Dominik Horn <horn.dominik101@yahoo.de>
2018-02-08 18:31:21 +0100

    Add Globals file and move Globals

---
+++ NotesApp/Data/Globals.swift
@@ -0,0 +1,12 @@
+//
+//  Globals.swift
+//  NotesApp
+//
+//  Created by Dominik Horn on 08.02.18.
+//  Copyright © 2018 Dominik Horn. All rights reserved.
+//
+
+import UIKit
+
+var currentColor = UIColor.blue
+var currentLineWidth: CGFloat = 2.0
